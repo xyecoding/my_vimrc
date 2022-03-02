f64f93db83ea254b6bf509fd246bef6257b43c30
xyegithub <xye@bupt.edu.cn>
2022-03-02 20:37:06 +0800

    new

--- install_awesome_vimrc.sh
+++ install_awesome_vimrc.sh
@@ -17,13 +17,16 @@ sudo apt install tmux -y
 echo -e "\033[31m config tmux \033[0m"
 cp ~/.vim_runtime/.tmux.conf ~
 
-echo -e "\033[31m installing miktex \033[0m"
-sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys D6BC243565B2087BC3F897C9277A7293F59E4889
-echo "deb http://miktex.org/download/ubuntu focal universe" | sudo tee /etc/apt/sources.list.d/miktex.list
-sudo apt-get update
-sudo apt-get install miktex -y
-miktexsetup finish
-initexmf --set-config-value [MPM]AutoInstall=1
+# echo -e "\033[31m installing miktex \033[0m"
+# sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys D6BC243565B2087BC3F897C9277A7293F59E4889
+# echo "deb http://miktex.org/download/ubuntu focal universe" | sudo tee /etc/apt/sources.list.d/miktex.list
+# sudo apt-get update
+# sudo apt-get install miktex -y
+# miktexsetup finish
+# initexmf --set-config-value [MPM]AutoInstall=1
+
+echo -e "\033[31m installing texlive \033[0m"
+sudo apt-get install texlive-full
 
 echo -e "\033[31m installing neovim \033[0m"
 sudo add-apt-repository ppa:neovim-ppa/stable
